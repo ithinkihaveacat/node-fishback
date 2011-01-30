ae38b0c8b302b707dadbe647454e0d8bdf86b846
Michael Stillwell <mjs@beebo.org>
2011-01-30 22:52:53 +0000

    Make the automatic header modification a bit safer.

--- run.js
+++ run.js
@@ -5,9 +5,11 @@ var fishback = require("fishback.js");
 var server = fishback.createServer();
 
 server.addResFilter(function (res) {
-    // If origin doesn't specify, make everything cachable for an hour
+    // If origin doesn't return a cache-control header, make everything cacheable 
+    // for 0 seconds.  This seems useless, but it means that if the the client 
+    // explicitly passes max-stale, it will get a cached response.
     if (!("cache-control" in res.headers)) {
-        res.headers["cache-control"] = "max-age=3600";
+        res.headers["cache-control"] = "max-age=0";
     }
 });
 
