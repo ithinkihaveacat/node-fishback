f56b63a6a483076ef8fe9aa4ef7dd554f6d4f1a2
Michael Stillwell <mjs@beebo.org>
2011-02-06 18:36:48 +0000

    Fix English.

--- run.js
+++ run.js
@@ -6,7 +6,7 @@ var server = fishback.createServer();
 
 server.addResFilter(function (res) {
     // If origin doesn't return a cache-control header, make everything cacheable 
-    // for 0 seconds.  This seems useless, but it means that if the the client 
+    // for 0 seconds.  This seems useless, but it means that if the client 
     // explicitly passes max-stale, it will get a cached response.
     if (!("cache-control" in res.headers)) {
         res.headers["cache-control"] = "max-age=0";
