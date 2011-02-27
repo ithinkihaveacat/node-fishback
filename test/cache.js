06c743af5bb594d2e0e61035a408900b079d2a35
Michael Stillwell <mjs@beebo.org>
2011-02-27 23:01:45 +0000

    Check that after 120 seconds, previously cached content becomes uncached.

--- test/cache.js
+++ test/cache.js
@@ -1,17 +1,59 @@
 var lib = require("./lib");
 
+var NOW = 198025200000;
+
 var response = { headers: { "cache-control": "max-age=60, public" }, body: "Hello, World" };
-var expected = [
-	{ headers: { "x-cache": "MISS", "cache-control": "max-age=60, public" }, body: "Hello, World" },
-	{ headers: { "x-cache": "HIT",  "cache-control": "max-age=60, public" }, body: "Hello, World" },
-	{ headers: { "x-cache": "HIT",  "cache-control": "max-age=60, public" }, body: "Hello, World" }
+
+var expected_miss = [
+    { headers: { "x-cache": "MISS", "cache-control": "max-age=60, public" }, body: "Hello, World" },
+    { headers: { "x-cache": "HIT",  "cache-control": "max-age=60, public" }, body: "Hello, World" },
+    { headers: { "x-cache": "HIT",  "cache-control": "max-age=60, public" }, body: "Hello, World" }
+];
+
+var expected_hit = [
+    { headers: { "x-cache": "HIT",  "cache-control": "max-age=60, public" }, body: "Hello, World" },
+    { headers: { "x-cache": "HIT",  "cache-control": "max-age=60, public" }, body: "Hello, World" },
+    { headers: { "x-cache": "HIT",  "cache-control": "max-age=60, public" }, body: "Hello, World" }
 ];
 
 var service = lib.createService(response);
 
-service.request(expected.length, function (actual) {
-	for (var i = 0; i < actual.length; i++) {
-	    lib.responseEqual(actual[i], expected[i]);
-	}
-	service.shutdown();
-});
+lib.step([ 
+
+    function (callback) {
+
+        Date.prototype.getTime = function() {
+            return NOW;
+        }
+
+        service.request(expected_miss.length, function (actual) {
+            for (var i = 0; i < actual.length; i++) {
+                lib.responseEqual(actual[i], expected_miss[i]);
+            }
+            callback();
+        });
+
+    },
+
+    // Should get a cache miss the first time, because we're 120 seconds
+    // on.
+    function (callback) {
+
+        Date.prototype.getTime = function() {
+            return NOW + 120000;
+        }
+
+        service.request(expected_miss.length, function (actual) {
+            for (var i = 0; i < actual.length; i++) {
+                lib.responseEqual(actual[i], expected_miss[i]);
+            }
+            callback();
+        });
+
+    },
+
+    function (callback) {
+        service.shutdown();
+    }
+
+]);
