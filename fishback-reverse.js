0e0ce46565f90f5a9bfce3bfad6711e4c798ebcf
Michael Stillwell <mjs@beebo.org>
2011-03-05 15:47:24 +0000

    Add example reverse proxy server.

---
+++ fishback-reverse.js
@@ -0,0 +1,18 @@
+#!/usr/local/bin/node
+
+/*
+ * Example of a reverse proxy server.  i.e. the sort of proxy server you might
+ * put in front of a HTTP server, to deliver cached responses to clients.
+ */
+
+require.paths.unshift("lib");
+
+var fishback = require("fishback");
+
+var server = fishback.createServer();
+
+server.addReqFilter(function (req) {
+    req.url = "http://localhost" + req.url;
+});
+
+server.listen();
