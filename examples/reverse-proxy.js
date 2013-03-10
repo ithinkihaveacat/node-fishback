17ad90446f3da013eed4063e11846a2e99af5da7
Michael Stillwell <mjs@beebo.org>
2013-03-10 17:31:21 +0000

    Add standard reverse proxy example

---
+++ examples/reverse-proxy.js
@@ -0,0 +1,34 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:false, strict:true, undef:true, unused:true, curly:true, node:true, indent:4, maxerr:50, globalstrict:true */
+
+"use strict";
+
+var fishback = require("../lib/fishback");
+var http = require("http");
+
+var PORT = 8080;
+
+var client = new fishback.Client("www.bbc.co.uk", "80");
+client.on('newRequest', function (req) {
+    console.info("CLIENT.newRequest " + req.method + " " + req.url);
+});
+client.on('newResponse', function (res) {
+    console.info("CLIENT.newResponse " + res.method + " " + res.url);
+});
+
+var proxy = fishback.createProxy(client);
+proxy.on('newRequest', function (req) {
+    console.info("PROXY.newRequest " + req.method + " " + req.url);
+});
+proxy.on('newResponse', function (res) {
+    console.info("PROXY.newResponse " + res.method + " " + res.url);
+}); 
+
+var server = new http.Server();
+server.on('request', proxy.request.bind(proxy));
+server.listen(PORT, function () {
+    console.info("Listening on port " + PORT);
+    console.info();
+    console.info("Try:");
+    console.info();
+    console.info("  $ curl -s -i http://localhost:" + PORT + "/");
+});
