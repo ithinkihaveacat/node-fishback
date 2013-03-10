5882f0b13e151f7732af27cea0c9fac8056fbc42
Michael Stillwell <mjs@beebo.org>
2013-03-10 17:31:02 +0000

    Move simple.js to caching-proxy.js

--- examples/caching-proxy.js
+++ examples/caching-proxy.js
@@ -1,19 +1,25 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:false, strict:true, undef:true, unused:true, curly:true, node:true, indent:4, maxerr:50, globalstrict:true */
+
+"use strict";
+
 var fishback = require("../lib/fishback");
 var http = require("http");
 
 var PORT = 8080;
+var BACKEND_HOST = "www.bbc.co.uk";
+var BACKEND_PORT = "80";
 
 var cache = new fishback.CacheMemory();
 cache.on('newRequest', function (req) {
     console.info("CACHE " + req.method + " " + req.url);
 });
 
-var client = new fishback.Client("www.bbc.co.uk", "80");
+var client = new fishback.Client(BACKEND_HOST, BACKEND_PORT);
 client.on('newRequest', function (req) {
     console.info("CLIENT " + req.method + " " + req.url);
 });
 
-var proxy = new fishback.Proxy(cache, client);
+var proxy = fishback.createCachingProxy(cache, client);
 proxy.on('newRequest', function (req) {
     console.info("PROXY " + req.method + " " + req.url);
 });
